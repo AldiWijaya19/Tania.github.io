c02aefe9ff870b5ebe8496605ff54593dc5e81ae
AldiWijaya19 <162130931+AldiWijaya19@users.noreply.github.com>
2024-03-04 23:02:04 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "I love you too Tante Nia !😘😘😘";
+  gif.src =
+"https://media.giphy.com/media/2dQ3FMaMFccpi/giphy.gif?cid=790b7611p730ay7unv6osss3cad3vrgtal3zlold63qw6mtd&ep=v1_gifs_search&rid=giphy.gif&ct=g";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
